318a04d99e8e6c54660eeb524d27ee780b4c9221
letulip <ivladimirskiy@ya.ru>
2017-10-19 09:48:51 +0300

    Установлен и подключен gulp плагин imagemin в gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -20,6 +21,15 @@ gulp.task("style", function() {
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
 });
 
 gulp.task("serve", ["style"], function() {
