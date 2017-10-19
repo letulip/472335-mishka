0edf21b3530cf859a3d0410121ac97ec46c608ff
letulip <ivladimirskiy@ya.ru>
2017-10-19 17:07:25 +0300

    Добавлен build: копирование файлов, сборка спрайтов, минификация стилей, оптимизация изображений

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgsprite = require("svg-sprite-generator");
+var sprite = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 var server = require("browser-sync").create();
 var run = require("run-sequence");
 var del = require("del");
@@ -21,6 +23,7 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe (minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
@@ -45,7 +48,25 @@ gulp.task("webp", function () {
 gulp.task("svgsprite", function () {
   // return del("build/img/sprite.svg")
   return gulp.src("img/icon-*.svg")
+  .pipe(svgsprite())
   .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("img/icon-*.svg")
+  // .pipe(svgmin(function (file) {
+  //   var prefix = path.basename(file.relative, path.extname(file.relative));
+  //   return {
+  //     plugins: [{
+  //       cleanupIDs: {
+  //         prefix: prefix + '-',
+  //         minify: true;
+  //       }
+  //     }]
+  //   }
+  // }))
+  .pipe(sprite())
   .pipe(gulp.dest("build/img"));
 });
 
@@ -53,7 +74,8 @@ gulp.task("copy", function () {
   return gulp.src([
     "fonts/**/*.{woff,woff2}",
     "img/*.{png,jpg,svg}",
-    "js/**"
+    "js/**",
+    "*.html"
     ], {
       base: "."
     })
@@ -70,13 +92,14 @@ gulp.task("build", function (done) {
     "style",
     "copy",
     "webp",
+    "sprite",
     done
   );
 });
 
 var defaultTask = ["build"];
 
-gulp.task("default", defaultTask);
+// gulp.task("default", defaultTask);
 
 gulp.task("serve", ["style"], function() {
   server.init({
